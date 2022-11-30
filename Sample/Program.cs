b58d5aeb98d653fce37ca35d3b7365f8c304c8fc
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-11-30 07:35:14 +0530

    Sample Program on ForLoop

---
+++ Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sample
+{
+    class dowhileloopDemo
+    {
+        public static void Main()
+        {
+            // for loop begins when x=1
+            // and runs till x <=4
+            for (int x = 1; x <= 4; x++)
+                Console.WriteLine("GeeksforGeeks");
+        }
+    }
+
+}
